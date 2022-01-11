ca9f841670547e2e5d9e0711abe30557b06cf505
Nadira Ali <nadiraali188a@gmail.com>
2022-01-11 15:50:36 +0000

    next steps planned out

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Global Memory - variables
-let index = 0;
+// Variable in global memory
+let functionTrigger = 0;
 const startButton = document.getElementById("start-button")
-// const introPage = document.querySelector("#intro-page")
-
-
 
 
 // Array object with quiz questions, options and answers
@@ -43,15 +40,25 @@ let questionsArray = [
 // When the START button is clicked
 function startQuiz() {
 
-    if (index === 0) {
+    if (functionTrigger === 0) {
         let removeIntro = document.getElementById("intro-page");
         removeIntro.remove();
+        //call function to start timer here
     }
 
 
 
-
+    return;
 }
 
 // Event listener for START button to trigger the commencement of the quiz
 startButton.addEventListener("click", startQuiz);
+
+/* Next steps:
+    1) Start the timer
+    2) Render Questions
+    3) Function to verify answers
+    4) Function to render score
+    5) register score in local storage 
+    6) create and link high score html
+    7) render highscore
