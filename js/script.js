bee8408fe49a139a30c8b5990fd66a65c094d581
Nadira Ali <nadiraali188a@gmail.com>
2022-01-16 11:49:58 +0000

    refactored problem with rendering answers and final score

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-// Variable in global memory
+// Variables in global memory
 let index = 0;
 let totalTime = 100;
 let timeCountdown;
 let highScore;
+let highestScore = 100;
 const startButton = document.getElementById("start-button");
 const questionElement = document.getElementById("render-questions");
 const finalScore = document.getElementById("final-score");
@@ -18,6 +19,9 @@ let answer1 = document.createElement("button");
 let answer2 = document.createElement("button");
 let answer3 = document.createElement("button");
 let answer4 = document.createElement("button");
+let submitButton = document.querySelector("register-score");
+let form = document.getElementById("form");
+
 
 
 //function to remove the introductory page
@@ -83,6 +87,7 @@ function showResult() {
     else {
         questionElement.appendChild(wrongAnswer)
         setTimeout(function(){
+                questionElement.innerHTML = "";
             index++;
             if (totalTime >= 10) {
                 totalTime = totalTime -10;
@@ -106,7 +111,7 @@ function startTimer() {
     timeCountdown = setInterval(function() {
         totalTime --;
         timeElement.textContent = totalTime;
-        if (totalTime == 0) {
+        if (totalTime === 0) {
         displayScore();
         }
     }, 1000);    
@@ -120,6 +125,33 @@ function displayScore() {
     form.style.display = "block";
 };
 
+
+function saveUserInformation(){
+    let initialUser = document.querySelector("#initialUser").value;
+    let userScore = highestScore;
+
+    if (initialUser === "") {
+
+        alert("You must insert initials before proceeding!");
+
+    } else {
+        
+        scoresList = JSON.parse(localStorage.getItem("scores")) || [];
+        let scores = {
+        score: userScore,
+        initials: initialUser
+        };
+
+        scoresList.push(scores);
+        localStorage.setItem("scores", JSON.stringify(scoresList));
+
+    }
+};
+
+let scoresList = [];
+function renderFinalScore(){
+    scoresList = JSON.parse(localStorage.getItem("scores"));
+};
 
 // When the START button is clicked
 function startQuiz() {
@@ -137,7 +169,6 @@ function startQuiz() {
 
 // Event listener for START button to trigger the commencement of the quiz
 startButton.addEventListener("click", startQuiz);
-
 
 
 // Array object with quiz questions, options and answers
@@ -186,21 +217,42 @@ answer3.setAttribute("class", "answers-button");
 answer4.setAttribute("class", "answers-button");
 
 
-/*Next steps:
-    1) Start the timer
-    2) Render Questions
-    3) Function to verify answers
-    4) Function to render score
-    5) register score in local storage 
-    6) create and link high score html
-    7) render highscore */
+/* high-score page code*/
 
-/*Help:
+function showHighScores() {
+    let highestScore = JSON.parse(localStorage.getItem("scores")) || [];
 
-1) when start button clicked the timer starts but doesn't continue decrementing
-2) if wrong answer selected doesn't automatically switch to next question
-3) message for wrong answer appears at the top
-4) when i submit initials doesnt go to highscore page
-5) highscore page doesnt allow css style changes
+    highestScore.sort(function(a, b) {
+        return b.score - a.score;
+      });
 
-*/
+      highestScore.forEach(function(score) {
+        
+        let scoresList= document.createElement("li");
+        scoresList.textContent = score.initials + " - " + score.score;
+        
+        let info = document.getElementById("ol-highscores");
+        info.appendChild(scoresList);
+        
+      });
+}
+
+let clearHighScores = document.getElementById("clear-button")
+
+clearHighScores.addEventListener("click", function () {
+    localStorage.clear();
+    window.location.reload();
+})
+
+showHighScores();
+
+
+highScoreTrigger.addEventListener("click", renderFinalScore);
+form.style.display = "none";
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    saveUserInformation();
+    window.location.href="high-score.html";
+    
+})
